7acc6bef9433343e4188c9ea1ccfbcf8983b1fac
yongsheng <1023993420@qq.com>
2019-06-25 18:14:14 +0800

    create project

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="app">
+    <div class="input-container">
+      <span>视频链接：</span>
+      <label>
+        <input type="text" v-model="videoUrl">
+      </label>
+    </div>
+    <video ref="video" :src="videoUrl" controls width="600px"></video>
+    <CropTool
+      :duration="duration"
+      :playing="playing"
+      :currentPlayingTime="currentTime"
+      @play="playVideo"
+      @pause="pauseVideo"
+      @stop="stopVideo"
+    />
+  </div>
+</template>
+
+<script>
+import CropTool from "./components/CropTool.vue";
+
+export default {
+  name: "app",
+  components: {
+    CropTool
+  },
+  data() {
+    return {
+      videoUrl: "http://video.6chcdn.com/instaclip/video/S3otc2o4pw.mp4",
+      duration: 0,
+      playing: false,
+      currentTime: 0
+    };
+  },
+  mounted() {
+    const videoElement = this.$refs.video;
+    videoElement.ondurationchange = () => {
+      this.duration = videoElement.duration;
+    };
+    videoElement.onplaying = () => {
+      this.playing = true;
+    };
+    videoElement.onpause = () => {
+      this.playing = false;
+    };
+    videoElement.ontimeupdate = () => {
+      this.currentTime = videoElement.currentTime;
+    };
+  },
+  methods: {
+    seekVideo(seekTime) {
+      this.$refs.video.currentTime = seekTime;
+    },
+    playVideo(time) {
+      this.seekVideo(time);
+      this.$refs.video.play();
+    },
+    pauseVideo() {
+      this.$refs.video.pause();
+    },
+    stopVideo() {
+      this.$refs.video.pause();
+      this.$refs.video.currentTime = 0;
+    }
+  }
+};
+</script>
